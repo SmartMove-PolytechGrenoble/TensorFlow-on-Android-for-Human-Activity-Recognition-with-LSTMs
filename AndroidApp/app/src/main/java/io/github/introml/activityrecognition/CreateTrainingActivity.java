6d7b12621bd14f769246950d63b0ca7dc531040f
geourjoa <anthony.geourjon@e.ujf-grenoble.fr>
2018-02-08 22:08:43 +0100

    Work on training activity

--- AndroidApp/app/src/main/java/io/github/introml/activityrecognition/CreateTrainingActivity.java
+++ AndroidApp/app/src/main/java/io/github/introml/activityrecognition/CreateTrainingActivity.java
@@ -6,8 +6,12 @@ import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
 
 public class CreateTrainingActivity extends AppCompatActivity {
+
+    private Spinner movementSpinner;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -15,6 +19,16 @@ public class CreateTrainingActivity extends AppCompatActivity {
         setContentView(R.layout.activity_create_training);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
+
+        movementSpinner = (Spinner) findViewById(R.id.movementSpinner);
+
+        // Create an ArrayAdapter using the string array and a default spinner layout
+        ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(this,
+                R.array.movements, android.R.layout.simple_spinner_item);
+        // Specify the layout to use when the list of choices appears
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        // Apply the adapter to the spinner
+        movementSpinner.setAdapter(adapter);
     }
 
 }
