78ea9b1264ccd320279281804771f2c3b2c91267
Unknown <vincent.turrin@gmail.com>
2018-03-06 16:09:25 +0100

    format data update

--- formatdata.py
+++ formatdata.py
@@ -8,23 +8,21 @@ Created on Sat Mar  3 13:49:29 2018
 import os
 import pandas as pd
 
-os.chdir("/home/mesh/S10/Orga_Git/TensorFlow-on-Android-for-Human-Activity-Recognition-with-LSTMs/data")
-
-if(os.path.isfile("alldata.txt")):
-    os.remove("alldata.txt")
-
-f = open("alldata.txt","a+")
-columns = ['user','activity','timestamp', 'x-acc', 'y-acc', 'z-acc', 'x-rot', 'y-rot']
-last_stamp = 0
-adf = pd.DataFrame()
-
-for filename in os.listdir():
-    if filename.endswith(".log"):
-        fread = open(filename, "r")
-        fcontent = fread.read()
-        df = pd.read_csv(filename, header = None, names = columns)
-        df['timestamp'] += last_stamp
-        adf = pd.concat([adf, df], axis = 0)
-        last_stamp = adf.tail(1)['timestamp'].item()
-        #f.write(contents)
-        #print(filename)
+def gather_data_csv():
+    os.chdir("/home/mesh/S10/Orga_Git/TensorFlow-on-Android-for-Human-Activity-Recognition-with-LSTMs/data")
+    
+    if(os.path.isfile("alldata.txt")):
+        os.remove("alldata.txt")
+    
+    columns = ['user','activity','timestamp', 'x-acc', 'y-acc', 'z-acc', 'x-rot', 'y-rot']
+    last_stamp = 0
+    adf = pd.DataFrame()
+    
+    for filename in os.listdir():
+        if filename.endswith(".log"):
+            df = pd.read_csv(filename, header = None, names = columns)
+            df['timestamp'] += last_stamp
+            adf = pd.concat([adf, df], axis = 0)
+            last_stamp = adf.tail(1)['timestamp'].item()
+            
+    return adf
